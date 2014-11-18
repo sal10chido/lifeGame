6dee25c140986916c8ad4926d08116c1779285a5
carnal <srobles@tralix.com>
2014-11-18 15:00:18 -0600

    nuevos test y codigos de maximo 4 lineas por metodo, solo faltan los test de la wikipedia

--- src/lifeGame/LifeGame.java
+++ src/lifeGame/LifeGame.java
@@ -1,11 +1,12 @@
 package lifeGame;
 
 public class LifeGame {
+
+	private int[][] tablero;
+
+	private int[][] tableroResultante;
 	
-	private int[][]tablero;
-	
-	private int[][]tableroResultante;
-	
+
 	public void crearTablero(final int longX, final int longY) {
 		tablero = new int[longX][longY];
 		tableroResultante = new int[longX][longY];
@@ -16,32 +17,74 @@ public class LifeGame {
 	}
 
 	public void analizarTablero() {
-		int celulasVecinas = 0;
-		for(int x = 0; x < tablero.length; x++) {
-			for(int y = 0; y < tablero[x].length; y++) {
-				celulasVecinas += obtenerCelulasVecinas(x,y);
-				determinarViveoMuere(x,y,celulasVecinas);
-			}
+		for (int x = 0; x < tablero.length; x++) {
+			for (int y = 0; y < tablero[x].length; y++)
+				determinarViveoMuere(x, y, obtenerCelulasVecinas(x, y));
 		}
 	}
 
-	private int obtenerCelulasVecinas(int posX, int posY) {
-		// TODO Reglas de posicion
-		return 0;
+	private int obtenerCelulasVecinas(final int posX, final int posY) {
+		int celulasVecinas = getCelulasIzquierda(posX, posY);
+		celulasVecinas += getCelulasDerecha(posX, posY);
+		celulasVecinas += getCelulasArribaAbajo(posX, posY);
+		return celulasVecinas;
+	}
+	
+	private int getCelulasIzquierda(final int posX, final int posY) {
+		int celulasVecinas = getCelula(posX - 1, posY);// Izquierda
+		celulasVecinas += getCelula(posX - 1, posY - 1);// IzquierdaSuperior
+		celulasVecinas += getCelula(posX - 1, posY + 1);// IzquierdaInferior
+		return celulasVecinas;
+	}
+
+	private int getCelulasDerecha(final int posX, final int posY) {
+		int celulasVecinas = getCelula(posX + 1, posY - 1);// DerechaSuperior
+		celulasVecinas += getCelula(posX + 1, posY);// Derecha
+		celulasVecinas += getCelula(posX + 1, posY + 1);// DerechaInferior
+		return celulasVecinas;
+	}
+
+	private int getCelulasArribaAbajo(final int posX, final int posY) {
+		int celulasVecinas = getCelula(posX, posY - 1);// Superior
+		celulasVecinas += getCelula(posX, posY + 1);// Inferior
+		return celulasVecinas;
+	}
+	
+	private int getCelula(final int posX, final int posY) {
+		if (positionOutOfRange(posX, posY))
+			return 0;
+		return tablero[posX][posY];
+	}
+
+	private boolean positionOutOfRange(final int posX, final int posY) {
+		return posX < 0 || posY < 0 || posX >= tablero.length
+				|| posY >= tablero[posX].length;
 	}
 	
 	private void determinarViveoMuere(int posX, int posY, int celulasVecinas) {
-		if (tablero[posX][posY] == 1) {
-			if (celulasVecinas == 2 || celulasVecinas == 3) {
-				tableroResultante[posX][posY] = 1;
-			} else {
-				tableroResultante[posX][posY] = 0;
-			}
-		} else {
-			if (celulasVecinas == 3) {
-				tableroResultante[posX][posY] = 1;
-			}
+		if (isCelulaViva(posX, posY))
+			obtenerResultadoConVecinosDeCelulaViva(posX, posY, celulasVecinas);
+		else
+			obtenerResultadoConVecinosDeCelulaMuerta(posX, posY, celulasVecinas);
+	}
+	
+	private void obtenerResultadoConVecinosDeCelulaViva(int posX, int posY,
+			int celulasVecinas) {
+		if (celulasVecinas == 2 || celulasVecinas == 3)
+			tableroResultante[posX][posY] = 1;
+		else
+			tableroResultante[posX][posY] = 0;
+	}
+	
+	private void obtenerResultadoConVecinosDeCelulaMuerta(int posX, int posY,
+			int celulasVecinas) {
+		if (celulasVecinas == 3) {
+			tableroResultante[posX][posY] = 1;
 		}
+	}
+	
+	private boolean isCelulaViva(int posX, int posY) {
+		return tablero[posX][posY] == 1;
 	}
 
 	public int[][] getResultado() {
--- test/lifeGame/LifeGameTest.java
+++ test/lifeGame/LifeGameTest.java
@@ -16,9 +16,107 @@ public class LifeGameTest {
 	}
 
 	@Test
-	public void testUnaCelulaQueMuere() {
+	public void testUnaCelulaQueMuereSinVecinos() {
 		game.crearTablero(4,4);
 		game.setCelula(2,2);
+		game.analizarTablero();
+		tableroResultante = game.getResultado();
+		assertEquals(0, tableroResultante[2][2]);
+	}
+	
+	@Test
+	public void testUnaCelulaQueMuereConUnVecino() {
+		game.crearTablero(4,4);
+		game.setCelula(2,2);
+		game.setCelula(3,2);
+		game.analizarTablero();
+		tableroResultante = game.getResultado();
+		assertEquals(0, tableroResultante[2][2]);
+	}
+	
+	@Test
+	public void testUnaCelulaQueViveConDosVecinos() {
+		game.crearTablero(4,4);
+		game.setCelula(2,2);
+		game.setCelula(3,2);
+		game.setCelula(1,2);
+		game.analizarTablero();
+		tableroResultante = game.getResultado();
+		assertEquals(1, tableroResultante[2][2]);
+	}
+	
+	@Test
+	public void testUnaCelulaQueViveConTresVecinos() {
+		game.crearTablero(4,4);
+		game.setCelula(2,2);
+		game.setCelula(3,2);
+		game.setCelula(1,2);
+		game.setCelula(2,1);
+		game.analizarTablero();
+		tableroResultante = game.getResultado();
+		assertEquals(1, tableroResultante[2][2]);
+	}
+	
+	@Test
+	public void testUnaCelulaQueMuereConCuatroVecinos() {
+		game.crearTablero(4,4);
+		game.setCelula(2,2);
+		game.setCelula(3,2);
+		game.setCelula(1,2);
+		game.setCelula(2,1);
+		game.setCelula(2,3);
+		game.analizarTablero();
+		tableroResultante = game.getResultado();
+		assertEquals(0, tableroResultante[2][2]);
+	}
+	
+	
+	
+	@Test
+	public void testEspacioVacioSinVecinos() {
+		game.crearTablero(4,4);
+		game.analizarTablero();
+		tableroResultante = game.getResultado();
+		assertEquals(0, tableroResultante[2][2]);
+	}
+	
+	@Test
+	public void testEspacioVacioConUnVecino() {
+		game.crearTablero(4,4);
+		game.setCelula(3,2);
+		game.analizarTablero();
+		tableroResultante = game.getResultado();
+		assertEquals(0, tableroResultante[2][2]);
+	}
+	
+	@Test
+	public void testEspacioVacioConDosVecinos() {
+		game.crearTablero(4,4);
+		game.setCelula(3,2);
+		game.setCelula(1,2);
+		game.analizarTablero();
+		tableroResultante = game.getResultado();
+		assertEquals(0, tableroResultante[2][2]);
+	}
+	
+	@Test
+	public void testEspacioVacioConTresVecinos() {
+		game.crearTablero(4,4);
+		game.setCelula(3,2);
+		game.setCelula(1,2);
+		game.setCelula(2,1);
+		game.analizarTablero();
+		tableroResultante = game.getResultado();
+		assertEquals(1, tableroResultante[2][2]);
+	}
+	
+	@Test
+	public void testEspacioVacioConCuatroVecinos() {
+		game.crearTablero(4,4);
+		game.setCelula(3,2);
+		game.setCelula(1,2);
+		game.setCelula(2,1);
+		game.setCelula(2,3);
 		game.analizarTablero();
 		tableroResultante = game.getResultado();
 		assertEquals(0, tableroResultante[2][2]);
